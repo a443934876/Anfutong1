f0740ae1a7bced34dfe552cf8c0cb825b673ea8f
amnesiac <378163883@qq.com>
2018-01-07 11:31:12 +0800

    1

--- app/src/main/java/org/chinasafety/cqj/anfutong/view/widget/my_camera/MyCamera.java
+++ app/src/main/java/org/chinasafety/cqj/anfutong/view/widget/my_camera/MyCamera.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e516e68deac7aa5007f9cc18de89b37e91037c6aa64d2807fb13ebd18036f45
-size 8941
+oid sha256:4b26190b9568fd517955b0010be56d7f39d56d8d90c0a579fa5cce031775d932
+size 8855
